75b35f39329f1b5c8b9a5a774303f0d558489e9e
Davide <davidelao30@gmail.com>
2023-07-07 19:10:46 +0200

    Initial commit

---
+++ src/main/java/me/dovide/stuff/RandomStuffClient.java
@@ -0,0 +1,11 @@
+package me.dovide.stuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomStuffClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
